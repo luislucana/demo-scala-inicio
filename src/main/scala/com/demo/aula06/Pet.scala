6139e3e44cb8c86100ab4c50290bf2f24e271ec8
Luis Lucana <luislucana@gmail.com>
2020-02-27 01:17:06 -0300

    Criação dos arquivos iniciais.

--- src/main/scala/com/demo/aula06/Pet.scala
+++ src/main/scala/com/demo/aula06/Pet.scala
@@ -2,6 +2,7 @@ package com.demo.aula06
 
 /**
   * https://docs.scala-lang.org/overviews/scala-book/abstract-classes.html
+  * https://github.com/eugenp/tutorials/tree/master/core-scala/src/main/scala/com/baeldung/scala
   */
 class Pet {
 
